9b3d600b4a690327e8349d23d68d1e71a97d6b5a
Alex Petrov <i@petrov.com.ru>
2019-01-28 20:54:51 +0300

    add page 404

--- nodejs-express/app.js
+++ nodejs-express/app.js
@@ -13,6 +13,10 @@ app.get('/game', (req, res) => {
     res.send('Game page');
 });
 
+app.use((req, res, next) => {
+    res.status(404).send('Page no found!');
+});
+
 app.listen(3000, () => {
     console.log('Sever runs on the port 3000');
 });
