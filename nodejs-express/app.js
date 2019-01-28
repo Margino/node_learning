0ea41899f50ae8fc394d5f5993b773ae966eab90
Alex Petrov <i@petrov.com.ru>
2019-01-28 19:32:45 +0300

    Express Routes

--- nodejs-express/app.js
+++ nodejs-express/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app     = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Hello there!');
+});
+
+app.get('/game', (req, res) => {
+    res.send('Game page');
+});
+
 app.listen(3000, () => {
     console.log('Sever runs on the port 3000');
 });
