ce75024fc83b77d634365cceca4d95b281580b01
Alex Petrov <i@petrov.com.ru>
2019-02-17 12:56:34 +0300

    add static public files

--- traversy/index.js
+++ traversy/index.js
@@ -1,10 +1,48 @@
 'use strict';
 
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs   = require('fs');
 
-const logger = new Logger();
+const server = http.createServer(( req, res ) => {
+    if ( req.url === '/' ) {
+        fs.readFile(
+            path.join(__dirname, '/public/', 'index.html'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                });
+                res.end(content);
+            }
+        );
+    }
 
-logger.on('message', (data) => console.log(`Called Listener`, data));
+    if ( req.url === '/about' ) {
+        fs.readFile(
+            path.join(__dirname, '/public/', 'about.html'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                });
+                res.end(content);
+            }
+        );
+    }
 
-logger.log('Hello World!');
-logger.log('Hi there!');
+    if (req.url === '/api/users') {
+        const users = [
+            { name: 'John', age: 22 },
+            { name: 'Anrew', age: 19 },
+        ];
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+        res.end(JSON.stringify(users));
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
